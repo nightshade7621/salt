ab0689f0b77f4b016f82b431b1db5c2e54431ad4
Aryan Sinha <sinha.aryan03@gmail.com>
2024-06-06 16:03:15 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/ubports/community-ports/halium-generic-adaptation-build-tools build
+[ -d build ] || git clone https://gitlab.com/techyminati/halium-generic-adaptation-build-tools build
 ./build/build.sh "$@"
