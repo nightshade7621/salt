03ad2c39ccd4894476d5c792142543b0383e068f
nightshade7621 <145030712+nightshade7621@users.noreply.github.com>
2024-06-06 17:08:30 +0000

    spaced: switch to master branch

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 
-[ -d build ] || git clone https://gitlab.com/techyminati/halium-generic-adaptation-build-tools build
+[ -d build ] || git clone https://gitlab.com/techyminati/halium-generic-adaptation-build-tools -b master build
 ./build/build.sh "$@"
